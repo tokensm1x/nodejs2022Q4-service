ffdb74a2cdfc78bfd7c8be263da2794e0c450435
tokensm1x <tokensmix@gmail.com>
2023-01-31 22:30:15 +0300

    feat: swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document: any = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
+  );
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
