d5000a38a661c289eee84aeaa4b46bd2b8f297c4
tokensm1x <tokensmix@gmail.com>
2023-02-01 17:33:06 +0300

    feat:dotenv

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import * as yaml from 'js-yaml';
 import { readFile } from 'fs/promises';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +13,6 @@ async function bootstrap() {
     await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
   );
   SwaggerModule.setup('doc', app, document);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
